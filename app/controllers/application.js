b994f43d302747c501aac98f4942a3780223bbdd
J. Cohen <hostelmarketing@gmail.com>
2018-04-30 12:00:24 -0700

    Wire up toggling

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,10 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    toggleActive(monster) {
+      monster.toggleProperty('active');
+      console.log('toggled', monster);
+    }
+  }
 });
